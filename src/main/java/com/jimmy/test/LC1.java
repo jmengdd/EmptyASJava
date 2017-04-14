9a8fef1d798db9591eae85081dc84fe4fdabe855
Jimmy <jimmy@doordash.com>
2017-04-13 20:03:01 -0700

    intialized

---
+++ src/main/java/com/jimmy/test/LC1.java
@@ -0,0 +1,15 @@
+package com.jimmy.test;
+
+/**
+ * Created by jimmy on 4/13/17.
+ */
+
+public class LC1 {
+
+
+    public static void main(String[] args) {
+
+        System.out.print("Hello World!");
+
+    }
+}
